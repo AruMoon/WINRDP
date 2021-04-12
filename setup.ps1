38191b56a8f272fd796476cc73a57fd509ba3491
Aru Moon <anton17082003@gmail.com>
2021-04-12 17:50:33 +0500

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,3 @@
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-Set-ADAccountPassword -Identity runneradmin -Reset -NewPassword (ConvertTo-SecureString -AsPlainText "$env:NEW_PASS" -Force)
+Set-LocalUser -Name runneradmin -Password (ConvertTo-SecureString -AsPlainText $env:NEW_PASS -Force)
 & {$P = $env:TEMP + '\ngrok.zip' ; Invoke-WebRequest 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip' -OutFile $P ; Expand-Archive -Path $P -DestinationPath $env:Programfiles -Force ; Remote-Item $P}
